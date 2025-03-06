0f96f1acffef9e9113e7391835e765e1d54a1ef5
OliverM224 <116174987+OliverM224@users.noreply.github.com>
2025-03-06 12:15:53 +0000

    Finalised Code

--- twitter.py
+++ twitter.py
@@ -1,33 +1,28 @@
+import json
 import tweepy
-import os
+#importing libraries
 
-def post_tweet():
+def lambda_handler(event, context):
+    # TODO implement
 
-        client = tweepy.Client(
-                consumer_key=os.getenv("API_KEY"),
-                consumer_secret=os.getenv("SECRET_API_KEY"),
-                access_token=os.getenv("ACCESS_TOKEN"),
-                access_token_secret=os.getenv("SECRET_ACCESS_TOKEN")
-        ) 
-        tweet_text = "WARNING: The Ipswich Waterfront has been flooded. Residents are advised to avoid the area. Star Lane has been closed."
-
-        response = client.create_tweet(text=tweet_text)
-
-        return{
-                "statusCode": 200
-        }
-
-        print(response)
-
-post_tweet()
-
-"""
-AWS grab JSON data
     sensorJSON = json.dumps(event)#converting sensor data into dictionary format.
     sensorData = json.loads(sensorJSON)#converting inputted data from dictionary to string format accessible to python
 
-    print(sensorData['dateTime'])
-    print(sensorData['Location'])
-    print(sensorData['Severity'])
+    DateTime = sensorData['dateTime']
+    Location = sensorData['Location']
+    Severity = sensorData['Severity']
 
-"""
+    client = tweepy.Client(
+                consumer_key = ('4vXXR6mVX0FHY15qWqkRCFth2'),
+                consumer_secret=('5C70WZaj1xpnE0r9kZpGJBjiFPK1E28txIx6mi8eDZC4eJ7XYd'),
+                access_token=('1895139284509691904-j95JzD2lLIDypBk5jXmumwB7mnjLpX'),
+                access_token_secret=('Y1ZFR8FiaO2prStZSwzaaqabWNJ22xB3XLL6kg8APjADZ')
+        ) 
+    tweet_text = "Attention! There is a {} risk flooding alert close to the {}. \nTime of incident: {}".format(Severity, Location, DateTime) +""
+
+    response = client.create_tweet(text=tweet_text)
+    
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Program executed.')
+    }
